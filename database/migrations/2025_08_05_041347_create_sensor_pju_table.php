f1e5bb547ac058e255515dcee1e224c151138e34
Civer Yoshioka Acer <muarifin@pens.ac.id>
2025-08-05 11:21:13 +0700

    inisialisasi awal broker dan tabel

---
+++ database/migrations/2025_08_05_041347_create_sensor_pju_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sensor_pju', function (Blueprint $table) {
+            $table->id();
+             $table->integer('voltage');
+            $table->integer('lamp_state');
+            $table->integer('counter');
+            $table->integer('frequency');
+            $table->float('power_factor');
+            $table->timestamp('datetime');
+            $table->float('brightness');
+            $table->float('current');
+            $table->float('energy');
+            $table->integer('error_state');
+            $table->integer('node_id');
+            $table->float('power');
+            $table->float('temperature');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sensor_pju');
+    }
+};
